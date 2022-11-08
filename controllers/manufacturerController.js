cf462b6697a0a51974df65cf545396b60d459e96
moschidisg <mosxidismosxidis@gmail.com>
2022-11-08 13:34:31 +0200

    add get all manufacturers function

--- controllers/manufacturerController.js
+++ controllers/manufacturerController.js
@@ -1,5 +1,19 @@
+const async = require('async');
+
+const Category = require('../models/Category');
+const Manufacturer = require('../models/Manufacturer');
+
 exports.all_manufacturers = (req, res, next) => {
-    res.render('showcase', {
-        title: 'All manufacturers',
-    });
+    Manufacturer.find()
+        .sort([['name', 'ascending']])
+        .exec(function (err, list_manufacturers) {
+            if (err) {
+                return next(err);
+            }
+
+            res.render('showcase', {
+                title: 'All manufacturers',
+                list_manufacturers,
+            });
+        });
 };

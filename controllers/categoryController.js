e41de3c62c18844e4a5088e2c02794da0bfe05bd
moschidisg <mosxidismosxidis@gmail.com>
2022-11-08 19:59:59 +0200

    add manufacturer creation

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,5 @@
 const async = require('async');
+const { body, validationResult } = require('express-validator');
 
 const Category = require('../models/Category');
 const Car = require('../models/Car');
@@ -54,3 +55,42 @@ exports.category_create_get = (req, res, next) => {
         title: 'Create new category',
     });
 };
+
+exports.category_create_post = [
+    body('categoryName', 'Category name must be specified.')
+        .trim()
+        .isLength({ min: 3 })
+        .escape(),
+    body('categoryDescription').trim().isLength({ max: 50 }).escape(),
+    body('categoryImage')
+        .trim()
+        .optional({ checkFalsy: true })
+        .isURL()
+        .withMessage('Image must be a valid URL.'),
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            res.render('create_category_form', {
+                title: 'Create new category',
+                category: req.body,
+                errors: errors.array(),
+            });
+            return;
+        }
+
+        // Data is valid
+
+        const newCategory = new Category({
+            name: req.body.categoryName,
+            description: req.body.categoryDescription,
+            image: req.body.categoryImage,
+        });
+        newCategory.save((err) => {
+            if (err) {
+                return next(err);
+            }
+            res.redirect('/categories');
+        });
+    },
+];
--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -9,6 +9,8 @@ router.get('/', categoryController.all_categories);
 
 router.get('/create', categoryController.category_create_get);
 
+router.post('/create', categoryController.category_create_post);
+
 // Renders all cars corresponding to the given category
 router.get('/:id', categoryController.get_category);
 
